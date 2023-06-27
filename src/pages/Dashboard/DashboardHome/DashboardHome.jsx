cbd71b79a1ccd147ca25e414dd1a0759744b1f70
Shahadat-Git <noyon560@gmail.com>
2023-06-27 06:04:18 +0600

    dashboard home added

--- src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import profile from '../../../assets/profile.png'
+
 
 const DashboardHome = () => {
+    const { user } = useAuth();
     return (
-        <div>
-            <h3>This is dashboard home</h3>
+        <div className='mt-5'>
+            <div className='flex justify-center'>
+                <h3 className='text-5xl shadow-lg inline-block px-10 py-2 rounded-full'>Dashboard</h3>
+            </div>
+            <div className='flex flex-col items-center gap-2 justify-center mt-10'>
+                <img className='w-[200px] h-[200px] rounded-full' src={user && user?.photoURL ? user?.photoURL : profile} alt="" />
+                <p>Name :</p>
+                <h3 className='text-xl'>{user?.displayName}</h3>
+                <p>Email :</p>
+                <h3 className='text-xl'>{user?.email}</h3>
+            </div>
         </div>
     );
 };
