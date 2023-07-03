b1bbf9c7ce9deea5089cf57c40f520243d1e8bc4
Shahadat-Git <noyon560@gmail.com>
2023-07-03 23:09:01 +0600

    classes page added

--- src/hooks/useUserStatus.jsx
+++ src/hooks/useUserStatus.jsx
@@ -5,11 +5,11 @@ import axios from 'axios';
 import useAxiosSecure from './useAxiosSecure';
 
 const useUserStatus = () => {
-    const { user, loading } = useAuth();
+    const { user } = useAuth();
     const [axiosSecure] = useAxiosSecure();
     const { data: status, isLoading: isStatusLoading } = useQuery({
         queryKey: ['isStatus', user?.email],
-        enabled: !loading,
+        enabled: user !== null,
         queryFn: async () => {
             const res = await axiosSecure.get(`/users/status/${user?.email}`)
             return res.data;
--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import noImg from '../../assets/no-img.jpg';
+import useUserStatus from '../../hooks/useUserStatus';
+import useAuth from '../../hooks/useAuth';
+import { toast } from 'react-hot-toast';
 
 const Classes = () => {
+    const [classes, setClasses] = useState([]);
+    const [axiosSecure] = useAxiosSecure();
+    const [status] = useUserStatus();
+    const { user } = useAuth();
+    useEffect(() => {
+        const getData = async () => {
+            const res = await axiosSecure.get('/classes/approved')
+            const { data } = res;
+            setClasses(data)
+        }
+        getData();
+    }, [])
+
+    const handleSelect = (id) => {
+        if (!user) {
+            return toast.error('Please log in before selection the course');
+        }
+        // console.log(id)
+        const cartData = {
+            classId: id,
+            email: user.email,
+        }
+        axiosSecure.post('/carts', cartData)
+            .then(res => {
+                // console.log(res.data)
+                if (res.data.insertedId) {
+                    toast.success('Selected this class');
+                };
+            })
+    }
     return (
         <div>
-            <h3>This is classes page</h3>
+            <div className='flex justify-center items-center'>
+                <h3 className='text-xl mt-5 lg:text-5xl shadow-lg inline-block px-10 py-1 lg:py-4 rounded-full'>All Classes</h3>
+            </div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-5 my-10'>
+                {
+                    classes.length > 0 && classes.map(item => <div key={item._id}>
+                        <div className={`${item.seats == 0 ? 'bg-red-600' : 'bg-base-100 '} card w-full shadow-xl `}>
+                            <div className="p-10">
+                                <img src={item.img || noImg} className=" rounded-xl border-2 h-72 w-full object-cover" />
+                            </div>
+                            <div className="card-body ">
+                                <h2 className="text-xl font-semibold">Name : {item.name}</h2>
+                                <p className="text-md font-semibold"> Instructor name : {item.instructorName}</p>
+                                <p className="text-md font-semibold"> Available seats : {item.seats}</p>
+                                <p className="text-md font-semibold"> Price : {item.price}</p>
+                                <button onClick={() => handleSelect(item._id)} disabled={item.seats == 0 || status?.status === 'admin' || status?.status === 'instructor' && true} className='btn btn-success hover:bg-green-600'>Select</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+
+
         </div>
     );
 };
